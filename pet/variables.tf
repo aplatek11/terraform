790d1f5972e0f5027b3855bb97b3f08449dfff1d
Aaron Platek <aaron.platek11@outlook.com>
2025-05-20 15:26:13 -0400

    Finished training today on Terraform for pet training (05/20/2025)

---
+++ pet/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+    default = "pets.txt"
+}
+
+variable "content" {
+    default = "My favorit pet is Mrs. Whiskers"
+}
+
+variable "prefix" {
+    default = "Mrs"
+}
+
+variable "separator" {
+    default = "."
+}
+
+variable length {
+    default = 2
+}
